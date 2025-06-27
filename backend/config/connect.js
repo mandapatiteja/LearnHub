d0e82d4abd767c819af649f5c63539d845cd8966
mandapatiteja <tejamandapati658@gmail.com>
2025-06-27 22:46:50 +0530

    Update connect.js

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const dbURI = process.env.MONGODB_URI || 'mongodb+srv://username:password@cluster0.bzjtj.mongodb.net/'; // Replace with your actual URI
+const dbURI = process.env.MONGODB_URI || 'MongoDB URI'; // Replace with your actual URI
 
 const dbConnect = () => {
   mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
