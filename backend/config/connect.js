05bd2339b1f5ddf66232a2ae44a1dabc0df53ec0
vijaysmart043 <vsmart996@gmail.com>
2025-06-27 22:15:52 +0530

    LearnHub

---
+++ backend/config/connect.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://Sowjanya:Sowjanya@cluster0.bzjtj.mongodb.net/'; // Replace with your actual URI
+
+const dbConnect = () => {
+  mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    throw new Error(`Could not connect to MongoDB: ${err}`);
+  });
+}
+
+module.exports = dbConnect
